22c2bc53e985881b637bbbc7e4d773cf781d28fe
DarlynYami05 <dwverling@gmail.com>
2021-08-24 03:40:10 -0500

    sala lista

---
+++ static/js/sala.js
@@ -0,0 +1,16 @@
+
+var btnCamara = document.getElementById("btnCamara");
+var btnMicrofono = document.getElementById("btnMicrofono");
+var btnSalirLlamada = document.getElementById("btnSalirLlamada");
+
+btnCamara.onclick = function(event){
+    alert("Encender o apagar cámara")
+}
+
+btnMicrofono.onclick = function(event){
+    alert("Encender o apagar micrófono")
+}
+
+btnSalirLlamada.onclick = function(event){
+    alert("Salir de la sala")
+}

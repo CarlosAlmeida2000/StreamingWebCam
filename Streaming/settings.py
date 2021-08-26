bd7e4d3f9049186879cb89e25f584aa8652a3ac4
CarlosAlmeida2000 <carlos.almeida2017@uteq.edu.ec>
2021-08-25 19:30:15 -0500

    make it better

--- Streaming/settings.py
+++ Streaming/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "%3nt=x@*6^=^48cx8q4rlzg6tcoi$30b0vo-@10@#qgys%gw4m"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
